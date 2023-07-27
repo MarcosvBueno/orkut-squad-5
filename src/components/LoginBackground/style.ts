8427a65ba768fc620ba79ccea955518899d68870
talitha <132085026+fleurspirituelles@users.noreply.github.com>
2023-07-27 16:52:37 -0300

    style: update styles and image format for login background

--- src/components/LoginBackground/style.ts
+++ src/components/LoginBackground/style.ts
@@ -1,20 +1,21 @@
 import styled from "styled-components";
-import background_image from "../../assets/img/login-background.jpg";
+import background_image from "../../assets/img/login-background.svg";
 
 const StyledImage = styled.img`
-    width: 800px;
-    height: 598px;
-    flex-shrink: 0;
-    border-radius: 20px;
     background: url(${background_image}) lightgray 50% / cover no-repeat;
+    background-size: cover;
+    border-radius: 20px;
+    flex-shrink: 0;
+    height: 598px;
+    width: 800px;
 `;
 
 const StyledText = styled.h1`
-    position: absolute;
     bottom: 20px;
-    left: 20px;
     color: var(--bg-text);
     font-size: 24px;
+    left: 20px;
+    position: absolute;
 `;
 
 export { StyledImage, StyledText };
