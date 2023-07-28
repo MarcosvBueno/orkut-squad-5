4dc50181f8543a53b59e76ca6bd859e0043e89f9
talitha <132085026+fleurspirituelles@users.noreply.github.com>
2023-07-28 20:25:57 -0300

    fix: adjust footer positioning to ensure it remains at the bottom

--- src/components/Footer/style.ts
+++ src/components/Footer/style.ts
@@ -1,26 +1,26 @@
 import { styled } from "styled-components";
 
 export const FooterContainer = styled.footer`
-    background-color: var(--dark-30);
-    bottom: 0;
-    height: 48px;
-    left: 0;
-    padding: 10px;
-    position: fixed;
-    text-align: center;
-    width: 100%;
+  background-color: var(--dark-30);
+  bottom: 0;
+  height: 48px;
+  left: 0;
+  padding: 10px;
+  position: relative;
+  text-align: center;
+  width: 100%;
 
+  h2 {
+    color: var(--brand-color);
+    font-size: 18px;
+    font-weight: 400;
+    letter-spacing: 0px;
+    line-height: 24px;
+  }
+
+  @media (max-width: 768px) {
     h2 {
-        color: var(--brand-color);
-        font-size: 18px;
-        font-weight: 400;
-        letter-spacing: 0px;
-        line-height: 24px;
+      font-size: 14px;
     }
-
-    @media (max-width: 768px) {
-        h2 {
-            font-size: 14px;
-        }
-    }
+  }
 `;
