f392a8c3436f78f2cb4fbaf69cdc1d3579258e4f
talitha <132085026+fleurspirituelles@users.noreply.github.com>
2023-07-29 21:06:44 -0300

    chore: refactor style for responsiveness

--- src/components/LoginContainer/style.ts
+++ src/components/LoginContainer/style.ts
@@ -1,53 +1,53 @@
 import styled from "styled-components";
 
 const StyledContainer = styled.div`
-  align-items: center;
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  margin-top: 100px;
-  margin-bottom: 100px;
-
-  & > * {
-    margin-right: 30px;
-  }
-
-  & > *:last-child {
-    margin-right: 0;
-  }
-
-  @media (max-width: 576px) {
-    flex-direction: column;
-
-    & > * {
-      margin-right: 0;
-      margin-top: 20px;
-    }
-
-    & > *:first-child {
-      display: none;
-    }
-  }
-
-  @media (min-width: 577px) and (max-width: 768px) {
+    align-items: center;
+    display: flex;
     flex-direction: row;
+    justify-content: center;
+    margin-top: 95px;
+    margin-bottom: 95px;
 
     & > * {
-      margin-right: 20px;
-      margin-top: 0;
+        margin-right: 30px;
     }
 
-    & > *:first-child {
-      display: initial;
+    & > *:last-child {
+        margin-right: 0;
     }
-  }
 
-  @media (min-width: 769px) {
-    & > * {
-      margin-right: 20px;
-      margin-top: 0;
+    @media (max-width: 576px) {
+        flex-direction: column;
+
+        & > * {
+            margin-right: 0;
+            margin-top: 20px;
+        }
+
+        & > *:first-child {
+            display: none;
+        }
     }
-  }
+
+    @media (min-width: 577px) and (max-width: 768px) {
+        flex-direction: row;
+
+        & > * {
+            margin-right: 20px;
+            margin-top: 0;
+        }
+
+        & > *:first-child {
+            display: initial;
+        }
+    }
+
+    @media (min-width: 769px) {
+        & > * {
+            margin-right: 20px;
+            margin-top: 0;
+        }
+    }
 `;
 
 export { StyledContainer };
